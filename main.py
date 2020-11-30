f4c3efc1e6d433f3658c2a62bed21b45d4dfeb57
Dainokode <antoniodainok@gmail.com>
2020-11-30 13:34:44 +0100

    final commit

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
+from database import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+print(logo)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+while shift >= 38:
+    print("Choose a shift number between 1 and 38")
+    shift = int(input("Type the shift number:\n"))
 
 
 def caesar(plain_text, shift_amount, direction):
     output_text = ""
 
     for letter in plain_text:
-        if direction == "encode":
-            letter_index = alphabet.index(letter) + shift_amount
-        elif direction == "decode":
-            letter_index = alphabet.index(letter) - shift_amount
+        if letter in alphabet:
+            if direction == "encode":
+                letter_index = alphabet.index(letter) + shift_amount
+                output_text += alphabet[letter_index]
+            elif direction == "decode":
+                letter_index = alphabet.index(letter) - shift_amount
+                output_text += alphabet[letter_index]
         else:
-            print("You entered a wrong keyword, try typing 'encrypt' to encode a message or 'decrypt' to decode a message.")
+            output_text += letter
 
-        output_text += alphabet[letter_index]
-
-    print(f"The {direction}d text is {output_text}")
+    print(f"The {direction}d text is {output_text}\n")
 
 
 caesar(plain_text=text, shift_amount=shift, direction=direction)
+
+user_choice = input(
+    "Would you like to restart or stop the program? Type 'yes' or 'no'\n").lower()
+
+while user_choice != "no":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    while shift >= 38:
+        print("Choose a shift number between 1 and 38\n")
+        shift = int(input("Type the shift number:\n"))
+    caesar(plain_text=text, shift_amount=shift, direction=direction)
+    user_choice = input(
+        "Would you like to restart or stop the program? Type 'yes' or 'no'\n").lower()
